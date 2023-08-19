cf22b674415cdd6ee1fee00f3173a664f00445ed
Ubuntu <ubuntu@ip-172-31-32-203.ec2.internal>
2023-08-19 14:21:12 +0000

    deploy: network setup

--- frontend/src/store/axios.js
+++ frontend/src/store/axios.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
 axios.defaults.timeout = 10000
-// axios.defaults.baseURL = 'https://coomoney.herokuapp.com/api/v1'
-axios.defaults.baseURL = 'http://localhost:3000/api/v1'
+axios.defaults.baseURL = 'https://coomoney.nahemah.com/api/v1'
+// axios.defaults.baseURL = 'http://localhost:3000/api/v1'
+// axios.defaults.baseURL = process.env.API_URL
+// axios.defaults.baseURL = 'http://backend/api/v1'
 
 if (localStorage.getItem('token')) {
     let token = localStorage.getItem('token')
